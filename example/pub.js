dc07e4adc96d098fb468bb826bb1f9f89a829a2d
Phil Cockfield <phil@cockfield.net>
2015-12-11 21:50:05 +1300

    Clean up examples.

--- example/pub.js
+++ example/pub.js
@@ -1,8 +1,18 @@
+const R = require("ramda");
 const pubsub = require("../")("amqp://docker");
 
 
+// Create the event.
 const myEvent = pubsub.event("MyEvent");
-myEvent.publish({ hello:22 });
+const publish = (index) => myEvent.publish({ event: index });
 
 
+// Read in the command-line args, to determine how many events to fire.
+// eg. `npm run pub ...` will fire 3 events.
+const args = process.argv.slice(2).join(" ");
+const count = args.length || 1;
+R.times(publish, count)
+
+
+// Close the process.
 setTimeout(function() { process.exit(0) }, 300);
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-
-console.log("TODO - babel-polyfill only require if not already created");
-
 "use strict"
-require("babel-polyfill"); // NB: Required for ES6 generators with built code in `lib`.
+
+if (typeof(regeneratorRuntime) === "undefined") {
+  require("babel-polyfill"); // NB: Required for ES6 generators with built code in `lib`.
+}
+
 module.exports = require("./lib/main");
